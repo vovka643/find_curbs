edcbff81a6515cac392657dfc930a8f05e27fe83
Vladimir Ponomarev <vovka643@list.ru>
2021-11-17 23:30:28 +0300

    Add files via upload
    add new filter to file. Now i use just road points to find curbs instead all points

--- curbs.py
+++ curbs.py
@@ -1,12 +1,12 @@
 """
 Docstring for curbs.py
 
-This code detect curb points in LIDAR 3D point cloud. It takes input file with 
+This code detect curb points in LIDAR 3D point cloud. It takes input file with
 LIDAR 3D point cloud and write curb point cloud in .las file
 
-Arguments is 
-    - path to LIDAR 3d point cloud 
-    - path to output file name of .las file with curbs points  
+Arguments is
+    - path to LIDAR 3d point cloud
+    - path to output file name of .las file with curbs points
 """
 
 import os
@@ -32,18 +32,22 @@ if __name__ == "__main__":
 
         output_file = desktop + '\\borders_' + \
             datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")+'.las'
-    
-    # read input file
+
+# read input file
     plydata = plf.PlyData.read(input_file)
     cloud = np.ndarray((len(plydata['vertex']), 3))
 
     start_p = plydata['vertex'][0]
 
-    # transform cloud to local coordinates
+# transform cloud to local coordinates
+    road_cloud = []
+
     for i in range(len(plydata['vertex'])):
         v = plydata['vertex'][i]
         cloud[i] = [v['x'] - start_p['x'], v['y'] - start_p['y'],
                     v['z'] - start_p['z']]
+        if v['scalar_Label'] == 1:
+            road_cloud.append(cloud[i])
 
     max_x = cloud[:, 0].max()
     min_x = cloud[:, 0].min()
@@ -54,12 +58,7 @@ if __name__ == "__main__":
     max_z = cloud[:, 2].max()
     min_z = cloud[:, 2].min()
 
-    new_cloud = []
-    for p in cloud:
-        if (p[2] - min_z < 4):
-            new_cloud.append(p)
-
-    #fill the mesh, find max and min value of hight in each cell
+# fill the mesh, find max and min value of hight in each cell
     step_x = 0.1
     step_y = 0.1
 
@@ -67,7 +66,7 @@ if __name__ == "__main__":
     num_y = math.ceil((max_y - min_y)/step_y) + 1
     mesh = np.zeros((num_x, num_y, 2))
 
-    for p in new_cloud:
+    for p in road_cloud:
         nx = math.ceil((p[0] - min_x)/step_x)
         ny = math.ceil((p[1] - min_y)/step_y)
 
@@ -81,19 +80,19 @@ if __name__ == "__main__":
         if (mesh[nx, ny, 1] < p[2]):
             mesh[nx, ny, 1] = p[2]
 
-    #create and fill cloud with curbs points
+# create and fill cloud with curbs points
     curb_cloud = []
 
-    for p in new_cloud:
+    for p in road_cloud:
         nx = math.ceil((p[0] - min_x)/step_x)
         ny = math.ceil((p[1] - min_y)/step_y)
 
-        if (mesh[nx, ny, 1] - mesh[nx, ny, 0] > 0.1) and
-        (mesh[nx, ny, 1] - mesh[nx, ny, 0] < 0.2):
+        if ((mesh[nx, ny, 1] - mesh[nx, ny, 0] > 0.1) and
+                (mesh[nx, ny, 1] - mesh[nx, ny, 0] < 0.2)):
             curb_cloud.append((p[0] + start_p[0], p[1] + start_p[1],
                                p[2] + start_p[2]))
 
-    #write output .las file
+# write output .las file
     header = laspy.LasHeader(point_format=3, version="1.2")
 
     las = laspy.LasData(header)
